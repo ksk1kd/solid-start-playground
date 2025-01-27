7c8d503fdbf3d11da955ba48ec3488154328a768
Keisuke Ikeda <ksk1kd.dev@gmail.com>
2025-01-28 00:13:45 +0900

    add file routing setting

--- src/app.tsx
+++ src/app.tsx
@@ -1,5 +1,14 @@
+import { Router } from "@solidjs/router";
+import { FileRoutes } from "@solidjs/start/router";
+import { Suspense } from "solid-js";
 import "./app.css";
 
 export default function App() {
-  return <main class="container">Content</main>;
+  return (
+    <main class="container">
+      <Router root={(props) => <Suspense>{props.children}</Suspense>}>
+        <FileRoutes />
+      </Router>
+    </main>
+  );
 }
