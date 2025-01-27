b9c5c05c9664b4c6e7e63172721e8fa9449daee1
Keisuke Ikeda <ksk1kd.dev@gmail.com>
2025-01-28 01:10:26 +0900

    add data fetching

--- src/routes/blog(detail)/[id].tsx
+++ src/routes/blog(detail)/[id].tsx
@@ -1,6 +1,23 @@
-import { useParams } from "@solidjs/router";
+import { createAsync, query, useParams } from "@solidjs/router";
+
+type Blog = { title: string; id: string };
+
+const getBlog = query(async (id: string) => {
+  "use server";
+  const response = await fetch(`https://api.vercel.app/blog/${id}`);
+  return (await response.json()) as Blog;
+}, "blogs");
+
+export const route = {
+  preload: () => {
+    const params = useParams();
+    getBlog(params.id);
+  },
+};
 
 export default function BlogDetail() {
   const params = useParams();
-  return <h1>Blog {params.id}</h1>;
+  const blog = createAsync(() => getBlog(params.id));
+
+  return <h1>{blog()?.title}</h1>;
 }
--- src/routes/blog/(blog-list).tsx
+++ src/routes/blog/(blog-list).tsx
@@ -1,3 +1,33 @@
+import { createAsync, query } from "@solidjs/router";
+import { For } from "solid-js";
+
+type Blog = { title: string; id: string };
+
+const getBlogList = query(async () => {
+  "use server";
+  const response = await fetch("https://api.vercel.app/blog");
+  return (await response.json()) as Blog[];
+}, "blogs");
+
+export const route = {
+  preload: () => getBlogList(),
+};
+
 export default function BlogList() {
-  return <h1>Blog List</h1>;
+  const blogs = createAsync(() => getBlogList());
+
+  return (
+    <>
+      <h1>Blog List</h1>
+      <ul>
+        <For each={blogs()}>
+          {(blog) => (
+            <li>
+              <a href={`/blog/${blog.id}`}>{blog.title}</a>
+            </li>
+          )}
+        </For>
+      </ul>
+    </>
+  );
 }
